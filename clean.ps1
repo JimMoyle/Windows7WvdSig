cbfd11b148911a874b5186a25be72828e2e92456
JimMoyle <github@jimmoyle.com>
2020-04-29 19:06:13 +0100

    Removed cleanfile from repo

--- clean.ps1
+++ clean.ps1
@@ -2,12 +2,14 @@ $r = Get-AzResource | Where-Object { $_.ResourceGroupName -eq 'TestMandS' }
 
 $r | Where-object { $_.ResourceType -eq 'Microsoft.Compute/virtualMachines' } | Remove-AzResource -Force -Confirm:$false
 
-Get-AzResource | Where-Object { $_.ResourceGroupName -eq 'TestMandS' } | Remove-AzResource -Force -Confirm
+Get-AzResource | Where-Object { $_.ResourceGroupName -eq 'TestMandS' } | Remove-AzResource -Force -Confirm:$false
 
 $hostPoolName = "Windows7"
 
 Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
 
 Get-RdsAppGroup -TenantName JMoyle -HostPoolName $hostPoolName | Remove-RdsAppGroup
+
+Get-RdsSessionHost -TenantName JMoyle -HostPoolName $hostPoolName | Where-Object { $_.SessionHostName -like "*.jimmoyle.net" } | Remove-RdsSessionHost
 
 Get-RdsHostPool -TenantName JMoyle | Where-Object { $_.HostPoolName -eq $hostPoolName } | Remove-RdsHostPool
